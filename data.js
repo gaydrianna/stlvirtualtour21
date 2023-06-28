b199f552ccc1adda15aa94a44784ee99eb3681f1
gaydrianna <33139296+gaydrianna@users.noreply.github.com>
2023-06-28 12:17:30 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,323 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-outside-suny-new-paltz-library",
+      "name": "Outside SUNY New Paltz Library!",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.019982605889879324,
+          "pitch": -0.0510187892720495,
+          "rotation": 0,
+          "target": "1-library-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5677069262163279,
+          "pitch": 0.06888404715116181,
+          "title": "Welcome to Sojourner Truth Library!<br>",
+          "text": "<div><span></span>Meet Anne Deutsch, Research and Education Librarian.</div><div><br></div><div><span></span>I really enjoy getting to know students, helping you navigate\nthe library, and thinking through research projects together.<span>&nbsp; </span>If you see me in the library, or out on a trail, or around town please say hello! </div>"
+        }
+      ]
+    },
+    {
+      "id": "1-library-entrance",
+      "name": "Library Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5510605893397846,
+          "pitch": 0.04360145220883993,
+          "rotation": 0,
+          "target": "2-main-floor-right"
+        },
+        {
+          "yaw": -1.4654132784843235,
+          "pitch": 0.0032361220696763837,
+          "rotation": 0,
+          "target": "5-main-floor-left"
+        },
+        {
+          "yaw": -0.11132323815485456,
+          "pitch": 0.0792753599800502,
+          "rotation": 0,
+          "target": "3-main-floor-far-right"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8944201124288149,
+          "pitch": 0.029076508067472417,
+          "title": "Kim (she/her)<br>",
+          "text": "<div>Circulation Clerk</div><div><br></div><div>People can ask her about anything from directions to finding a book!</div><div><br></div><div><br></div><style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cellSehS</style>She enjoys<span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;I love adventure sports like biking and cross country skiing!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\"> adventure sports like biking and skiing!<br></span>"
+        },
+        {
+          "yaw": 0.24052366156325888,
+          "pitch": -0.01124900622442837,
+          "title": "Circulation Desk<br>",
+          "text": "<span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;here is where you can take out books, ask for directions, ask questions!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">Here is where you can take out books, ask for directions, ask questions!</span><style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style>"
+        },
+        {
+          "yaw": -0.7944002913570696,
+          "pitch": 0.09656407047995863,
+          "title": "Scanning Room!<br>",
+          "text": "<style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;Use the scanners to digitize a chapter or two of your textbook!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">Use the scanners to digitize a chapter or two of your textbook!</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-main-floor-right",
+      "name": "Main Floor Right",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6750565812730009,
+          "pitch": 0.0981326303915111,
+          "rotation": 0,
+          "target": "3-main-floor-far-right"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.484184552092792,
+          "pitch": 0.06069124275168036,
+          "title": "Reference Books<br>",
+          "text": "<style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;These are books to get you started on your research, they can't leave the library, but stop by the scanning room to remember you favorite finds.&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">These are books to get you started on your research, they can't leave the library, but stop by the scanning room to remember you favorite finds.</span>"
+        },
+        {
+          "yaw": 0.07822998290585303,
+          "pitch": -0.009086849968850785,
+          "title": "Meet Scoob<br>",
+          "text": "<div>Scoob is a Library Assistant! They can answer any question you have about Interlibrary Loan (our library system to share academic materials between libraries)!</div><div><br></div><div>Scoob really likes helping people find books in the stacks. They say, \"some of the call numbers are hard to understand. If you ever need help finding a book and I'm in the library, feel free to ask!</div><div><br></div><div>Fun fact: Has a tattoo that is a star from carebears on their upper arm!<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-main-floor-far-right",
+      "name": "Main Floor Far Right",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.7166110940786954,
+        "pitch": 0.2073145390657416,
+        "fov": 1.3268945324871384
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.30475720875553947,
+          "pitch": -0.027110426911381325,
+          "rotation": 0,
+          "target": "4-main-floor-far-left"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7539548397141225,
+          "pitch": 0.158384725955397,
+          "title": "Computers",
+          "text": "<style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;You can use these to access software only available on campus or remote desktop!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">You can use these to access software only available on campus or remote desktop!</span>"
+        },
+        {
+          "yaw": 2.38557294354114,
+          "pitch": -0.03200183794984923,
+          "title": "DASH Lab<br>",
+          "text": "<style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;The digital arts science and humanities lab is here for your podcasting, and project needs!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">The digital arts science and humanities lab is here for your podcasting, and digital project needs!</span>"
+        },
+        {
+          "yaw": 0.8369872179660298,
+          "pitch": 0.035561029133724276,
+          "title": "Meet Jen<br>",
+          "text": "<div>Jen is a Research &amp; Education Librarian. <br></div><div><br></div><div>One of her favorite questions to answer is: Where do I find a peer reviewed article on the history of bubble gum?</div><div><br></div><div>A fun fact: She is into gardening, and talking about gardening, especially growing food and native pollinators. <br></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-main-floor-far-left",
+      "name": "Main Floor Far Left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6668312300987385,
+          "pitch": 0.20833876939330764,
+          "rotation": 0,
+          "target": "5-main-floor-left"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9548879117359412,
+          "pitch": 0.17698636678095525,
+          "title": "Printers",
+          "text": "<style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;We have both color, and b + w printers!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">We have both color, and b + w printers!</span>"
+        },
+        {
+          "yaw": 2.23691656529655,
+          "pitch": 0.155311602840845,
+          "title": "Meet Emily!<br>",
+          "text": "<div>Emily is a graduate student assistant!</div><div><br></div><div>Her favorite questions to answer are about how to navigate the library website.</div><div><br></div><div>Fun fact: She teaches zumba and merengue music<br></div>"
+        },
+        {
+          "yaw": 1.2281250553405023,
+          "pitch": 0.10198814893851171,
+          "title": "Meet Chrissy<br>",
+          "text": "<div>Chrissy is a Research &amp; Education Librarian.</div><div><br></div><div>She likes supporting research about environmental studies, english, and asian studies. She enjoys researching about farming, chickens and birds. <br></div><div><br></div><div>Fun Fact: She loves cats, and her favorite color is plum purple!<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-main-floor-left",
+      "name": "Main Floor Left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.8482479248379136,
+        "pitch": 0.2533541586514687,
+        "fov": 1.3268945324871384
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.80282467075732,
+          "pitch": 0.024024972898928354,
+          "rotation": 0,
+          "target": "0-outside-suny-new-paltz-library"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5918292685532274,
+          "pitch": 0.13682395131142044,
+          "title": "Meet Madeline<br>&nbsp;and the Zine Library!<br>",
+          "text": "<div>Madeline is your friendly zine librarian! She'd like to tell you a bit about herself:</div><div><br></div><div>\"<span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;I buy books, films, (and zines!) for the Library. Let me know what we can get to support your reading / viewing / research. I also work as a liaison to the Art and Art History Departments, and I do some library instruction and research education at the Reference Desk.&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:577,&quot;3&quot;:{&quot;1&quot;:0},&quot;9&quot;:0,&quot;12&quot;:0}\">I buy books, films, (and zines!) for the Library. Let me know what we can get to support your reading / viewing / research. I also work as a liaison to the Art and Art History Departments, and I do some library instruction and research education at the Reference Desk.</span></div><div><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;I buy books, films, (and zines!) for the Library. Let me know what we can get to support your reading / viewing / research. I also work as a liaison to the Art and Art History Departments, and I do some library instruction and research education at the Reference Desk.&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:577,&quot;3&quot;:{&quot;1&quot;:0},&quot;9&quot;:0,&quot;12&quot;:0}\"><br></span></div><div><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;I buy books, films, (and zines!) for the Library. Let me know what we can get to support your reading / viewing / research. I also work as a liaison to the Art and Art History Departments, and I do some library instruction and research education at the Reference Desk.&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:577,&quot;3&quot;:{&quot;1&quot;:0},&quot;9&quot;:0,&quot;12&quot;:0}\">I like to make zines about humans, information systems, and feelings!\"</span><style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style></div>"
+        },
+        {
+          "yaw": -1.7383333118317452,
+          "pitch": -0.029236019022663484,
+          "title": "Study Rooms<br>",
+          "text": "<style type=\"text/css\">td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}</style><span style=\"font-size:10pt;font-family:Arial;font-style:normal;\" data-sheets-value=\"{&quot;1&quot;:2,&quot;2&quot;:&quot;Working on a project, meet with your group here!&quot;}\" data-sheets-userformat=\"{&quot;2&quot;:513,&quot;3&quot;:{&quot;1&quot;:0},&quot;12&quot;:0}\">Working on a project, meet with your group here!</span>"
+        },
+        {
+          "yaw": 3.1373079912243007,
+          "pitch": 0.12155522552249032,
+          "title": "A Message from your tour creator Adrianna!<br>",
+          "text": "<div>Thank you for going on this adventure with us! <br></div><div><br></div><div>My name is Adrianna and I am a Research &amp; Education Librarian. My pronouns are she/they and if you see me around(and sometimes even if you can't!) you can ask me about any topic you'd like to research! I especially love exploring intersectionality, digital humanities and culture. <br></div><div><br></div><div>Turn back to the beginning of the tour with this arrow to check out anything you might have missed!</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
